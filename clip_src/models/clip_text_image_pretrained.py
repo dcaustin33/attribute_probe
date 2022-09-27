b9a3c8411923ce83fba289555549fa7aa020c670
dcaustin33 <dcaustin33@gmail.com>
2022-09-27 21:19:41 +0000

    Added BYOL and concatenation

--- clip_src/models/clip_text_image_pretrained.py
+++ clip_src/models/clip_text_image_pretrained.py
@@ -28,3 +28,55 @@ class CLIP_text_image(nn.Module):
 
 
         return (binary_pred1, classifier_pred1), outputs.logits_per_image
+
+
+class CLIP_text_image_concat(nn.Module):
+
+    def __init__(self, args = None):
+        super().__init__()
+        self.args = args
+        self.clip = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.linear1 = nn.ModuleList([nn.Linear(1024, 1) for i in range(312)])
+        self.linear2 = nn.ModuleList([nn.Linear(768 + 512, 1) for i in range(312)])
+        self.classifier1 = nn.ModuleList([nn.Sequential(nn.Linear(1024, 312), nn.ReLU(), nn.BatchNorm1d(312), nn.Linear(312, 1)) for i in range(312)])
+        self.classifier2 = nn.ModuleList([nn.Sequential(nn.Linear(768 + 512, 312), nn.ReLU(), nn.BatchNorm1d(312), nn.Linear(312, 1)) for i in range(312)])
+
+    def forward(self, prompts, images):
+        text = prompts
+        inputs = self.processor(text=text, return_tensors="pt", padding=True)
+        for i in inputs:
+            inputs[i] = inputs[i].cuda()
+        inputs['pixel_values'] = images.cuda()
+        outputs = self.clip(**inputs)
+
+        image_embed = outputs.image_embeds
+        image_out = outputs.vision_model_output['pooler_output']
+        text_embed = outputs.text_embeds
+        text_out = outputs.text_model_output['pooler_output']
+
+        classifications = []
+
+        for i in range(text_embed.size(0)):
+            new_text_embed = text_embed[i].unsqueeze(0).repeat(image_embed.size(0), 1)
+            new_text_out = text_out[i].unsqueeze(0).repeat(image_out.size(0), 1)
+            final_embed = torch.cat((image_embed, new_text_embed), dim=1)
+            final_out = torch.cat((image_out, new_text_out), dim=1)
+
+            lin1 = self.linear1[i](final_embed)
+            class1 = self.classifier1[i](final_embed)
+
+            lin2 = self.linear2[i](final_out)
+            class2 = self.classifier2[i](final_out)
+
+            inter_class = [lin1, lin2, class1, class2]
+            if i == 0:
+                classifications = inter_class
+
+            else:
+                classifications[0] = torch.cat((classifications[0], inter_class[0]), dim=1)
+                classifications[1] = torch.cat((classifications[1], inter_class[1]), dim=1)
+                classifications[2] = torch.cat((classifications[2], inter_class[2]), dim=1)
+                classifications[3] = torch.cat((classifications[3], inter_class[3]), dim=1)
+
+        return classifications, outputs.logits_per_image
