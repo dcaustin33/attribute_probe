f2f4149e68713132bee04e86589330cda530f493
dcaustin33 <dcaustin33@gmail.com>
2023-03-26 16:31:21 +0000

    Generation commits

--- mit_states_src/training_scripts/inversion/individual_token_per_image/run_token_per_im.sh
+++ mit_states_src/training_scripts/inversion/individual_token_per_image/run_token_per_im.sh
@@ -1,15 +1,14 @@
-python3 train_weird_texture.py -train  --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_sti_real --name human_4_leg/outputs_texture_sti_real --steps 3;
-python3 train_with.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_sti_real --name human_4_leg/outputs_texture_sti_real --steps 3;
-python3 train_stitched_concept.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_sti_real --name human_4_leg/outputs_texture_sti_real --steps 3;
+python3 train_weird_texture.py -train  --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_texture --name human_4_leg/outputs_texture_texture --steps 3000;
+python3 train_with.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_with --name human_4_leg/outputs_texture_with  --steps 3000;
+#python3 train_stitched_concept.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_sti_real --name human_4_leg/outputs_texture_sti_real --steps 3000;
 
-python3 train_weird_texture.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_sti_real --name mandarin_fish/outputs_texture_sti_real --steps 3;
-python3 train_with.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_sti_real --name mandarin_fish/outputs_texture_sti_real --steps 3;
-python3 train_stitched_concept.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_sti_real --name mandarin_fish/outputs_texture_sti_real --steps 3;
+python3 train_weird_texture.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_texture --name mandarin_fish/outputs_texture_texture --steps 3000;
+python3 train_with.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_with --name mandarin_fish/outputs_texture_with --steps 3000;
+#python3 train_stitched_concept.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_sti_real --name mandarin_fish/outputs_texture_sti_real --steps 3000;
 
-python3 train_weird_texture.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_sti_real --name worms/outputs_texture_sti_real --steps 3;
-python3 train_with.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_sti_real --name worms/outputs_texture_sti_real --steps 3;
-python3 train_stitched_concept.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_sti_real --name worms/outputs_texture_sti_real --steps 3;
+python3 train_weird_texture.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_texture --name worms/outputs_texture_texture --steps 3000;
+python3 train_with.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_with --name worms/outputs_texture_with --steps 3000;
+#python3 train_stitched_concept.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_sti_real --name worms/outputs_texture_sti_real --steps 3000;
 
 
-
-# sudo shutdown -h;
+sudo shutdown -h;
