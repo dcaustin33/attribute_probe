f0983827ee91de02ee9654944c73c402c4ccab0e
dcaustin33 <dcaustin33@gmail.com>
2023-04-23 23:18:57 +0000

    Inversion commits

--- mit_states_src/training_scripts/inversion/individual_token_per_image/run_token_per_im.sh
+++ mit_states_src/training_scripts/inversion/individual_token_per_image/run_token_per_im.sh
@@ -1,30 +1,17 @@
-#python3 train_weird_texture.py -train  --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_texture --name human_4_leg/outputs_texture_texture --steps 3000;
-#python3 train_with.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_with --name human_4_leg/outputs_texture_with  --steps 3000;
-#python3 train_stitched_concept.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_sti_real --name human_4_leg/outputs_texture_sti_real --steps 3000;
-#python3 train_style.py -train --data_dir human_4_leg/stitched_realistic --output_dir human_4_leg/outputs_texture_style --name human_4_leg/outputs_texture_style --steps 3000;
+python3 train_style.py -train --data_dir coral/beard_pics --output_dir coral/outputs_beard_style --name coral/outputs_beard_style_model --steps 3000;
+python3 train_style.py -train --data_dir coral/solo_pics --output_dir coral/outputs_solo_style --name coral/outputs_solo_style_model --steps 3000;
+python3 train_weird_texture.py -train --data_dir coral/beard_pics --output_dir coral/outputs_beard_texture --name coral/outputs_beard_texture_model --steps 3000;
+python3 train_weird_texture.py -train --data_dir coral/solo_pics --output_dir coral/outputs_solo_texture --name coral/outputs_solo_texture_model --steps 3000;
 
-#python3 train_weird_texture.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_texture --name mandarin_fish/outputs_texture_texture --steps 3000;
-#python3 train_with.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_with --name mandarin_fish/outputs_texture_with --steps 3000;
-#python3 train_stitched_concept.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_sti_real --name mandarin_fish/outputs_texture_sti_real --steps 3000;
-#python3 train_style.py -train --data_dir mandarin_fish/tshirt_stitches --output_dir mandarin_fish/outputs_texture_style --name mandarin_fish/outputs_texture_style --steps 3000;
+python3 train_style.py -train --data_dir barnacles/beard_pics --output_dir barnacles/outputs_beard_style --name barnacles/outputs_beard_style_model --steps 3000;
+python3 train_style.py -train --data_dir barnacles/solo_pics --output_dir barnacles/outputs_solo_style --name barnacles/outputs_solo_style_model --steps 3000;
+python3 train_weird_texture.py -train --data_dir barnacles/beard_pics --output_dir barnacles/outputs_beard_texture --name barnacles/outputs_beard_texture_model --steps 3000;
+python3 train_weird_texture.py -train --data_dir barnacles/solo_pics --output_dir barnacles/outputs_solo_texture --name barnacles/outputs_solo_texture_model --steps 3000;
 
-#python3 train_weird_texture.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_texture --name worms/outputs_texture_texture --steps 3000;
-#python3 train_with.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_with --name worms/outputs_texture_with --steps 3000;
-#python3 train_stitched_concept.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_sti_real --name worms/outputs_texture_sti_real --steps 3000;
-#python3 train_style.py -train --data_dir worms/stitched_realistic_worms --output_dir worms/outputs_texture_style --name worms/outputs_texture_style --steps 3000;
-
-python3 train_style.py -train --data_dir ladybugs/hair_pics --output_dir ladybugs/outputs_texture_style --name ladybugs/outputs_texture_style --steps 3000;
-python3 train_style.py -train --data_dir lichen/hair_pics --output_dir lichen/outputs_texture_style --name lichen/outputs_texture_style --steps 3000;
-python3 train_style.py -train --data_dir ants/hair_pics --output_dir ants/outputs_texture_style --name ants/outputs_texture_style --steps 3000;
-
-python3 train_with.py -train --data_dir ladybugs/hair_pics --output_dir ladybugs/outputs_texture_with --name ladybugs/outputs_texture_with --steps 3000;
-python3 train_with.py -train --data_dir lichen/hair_pics --output_dir lichen/outputs_texture_with --name lichen/outputs_texture_with --steps 3000;
-python3 train_with.py -train --data_dir ants/hair_pics --output_dir ants/outputs_texture_with --name ants/outputs_texture_with --steps 3000;
-
-python3 train_weird_texture.py -train --data_dir ladybugs/hair_pics --output_dir ladybugs/outputs_texture_texture --name ladybugs/outputs_texture_texture --steps 3000;
-python3 train_weird_texture.py -train --data_dir lichen/hair_pics --output_dir lichen/outputs_texture_texture --name lichen/outputs_texture_texture --steps 3000;
-python3 train_weird_texture.py -train --data_dir ants/hair_pics --output_dir ants/outputs_texture_texture --name ants/outputs_texture_texture --steps 3000;
-
+python3 train_style.py -train --data_dir lichen/beard_pics --output_dir lichen/outputs_beard_style --name lichen/outputs_beard_style_model --steps 3000;
+python3 train_style.py -train --data_dir lichen/solo_pics --output_dir lichen/outputs_solo_style --name lichen/outputs_solo_style_model --steps 3000;
+python3 train_weird_texture.py -train --data_dir lichen/beard_pics --output_dir lichen/outputs_beard_texture --name lichen/outputs_beard_texture_model --steps 3000;
+python3 train_weird_texture.py -train --data_dir lichen/solo_pics --output_dir lichen/outputs_solo_texture --name lichen/outputs_solo_texture_model --steps 3000;
 
 
 sudo shutdown -h;
